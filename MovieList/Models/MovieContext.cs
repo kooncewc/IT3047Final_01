714d69c9d3264415370780fd36bfa50db07bcc44
kooncewc <kooncewc@mail.uc.edu>
2023-04-27 17:30:43 -0400

    Add project files.

---
+++ MovieList/Models/MovieContext.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieList.Models;
+
+namespace MovieList.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie {
+                    MovieId = 4,
+                    Name = "Casablanca",
+                    Year = 1943,
+                    Rating = 5,
+                    GenreId = "D"
+                },
+                new Movie {
+                    MovieId = 2,
+                    Name = "Wonder Woman",
+                    Year = 2017,
+                    Rating = 3,
+                    GenreId = "A"
+                },
+                new Movie {
+                    MovieId = 3,
+                    Name = "Moonstruck",
+                    Year = 1988,
+                    Rating = 4,
+                    GenreId = "R"
+                }
+            );
+        }
+    }
+}
